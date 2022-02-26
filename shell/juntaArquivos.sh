1922c2a94a14a34074a68e7202029861cc76db31
Ivaney Vieira <ivaneyvieira@gmail.com>
2022-02-26 19:38:35 -0300

    Inicial

--- shell/juntaArquivos.sh
+++ shell/juntaArquivos.sh
@@ -4,3 +4,12 @@ pv dados/estabelcimentos/*.ESTABELE > dados/estabelcimentos/tudojunto.txt
 pv dados/empre/*.EMPRECSV > dados/empre/tudoJunto.txt
 pv dados/tabelas/F.K03200\$Z.D20108.CNAECSV > dados/tabelas/cnae.txt
 
+(
+    CNPJ_BASICO                 TEXT,
+    RASAO_SOCIAL                TEXT,
+    NATUREZA_JURIDICA           TEXT,
+    QUALIFICACAO_DO_RESPONSAVEL TEXT,
+    CAPITAL_SOCIAL              TEXT,
+    PORTE                       TEXT,
+    ENTE_FEDERATIVO_RESPONSAVEL TEXT
+);
